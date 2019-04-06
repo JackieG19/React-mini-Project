071ec720af97e8978f00bb98e627e6f785b15ed0
Jackie <jacgut19@gmail.com>
2019-04-06 17:55:51 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Profile from "./component/Profile";
 
 class App extends Component {
+  state = {
+    firstname: null,
+    lastname: null,
+    email: null
+  }
+  
+  handleCLick = () =>{
+    console.log("button clicked") // button works
+  }
+  setFirstName = e => {
+    console.log(e.target.value);
+    this.setState({
+      firstname: e.target.value
+    });
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Profile/>
+        <p>My App</p>
+        <input onChange={this.setFirstName} type="text" placeholder="FirstName" />
+        <button onClick={this.handleCLick}>submit</button>
       </div>
     );
   }
